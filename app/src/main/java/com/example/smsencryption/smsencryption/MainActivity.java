96aba1a2ce01d598cf2abd31496bbdff69afb0c5
Joana Chavez <chavezlucero88@gmail.com>
2017-04-14 01:19:41 +0200

    fixing emulation

--- app/src/main/java/com/example/smsencryption/smsencryption/MainActivity.java
+++ app/src/main/java/com/example/smsencryption/smsencryption/MainActivity.java
@@ -27,17 +27,7 @@ public class MainActivity extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
-
         txtPhoneNumber = (EditText) findViewById(R.id.txtPhoneNumber);
-
 
         btnStart = (Button) findViewById(R.id.btnStart);
         btnStart.setOnClickListener(new View.OnClickListener(){
