e77f3e303343953e5e920663733cd0dc64ac9849
Joana Chavez <chavezlucero88@gmail.com>
2017-04-13 23:12:42 +0200

    adding the first components into the mainactivity

--- app/src/main/java/com/example/smsencryption/smsencryption/MainActivity.java
+++ app/src/main/java/com/example/smsencryption/smsencryption/MainActivity.java
@@ -1,15 +1,24 @@
 package com.example.smsencryption.smsencryption;
 
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button btnStart;
+    EditText txtPhoneNumber;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,6 +34,44 @@ public class MainActivity extends AppCompatActivity {
                 Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
                         .setAction("Action", null).show();
             }
+        });
+
+        txtPhoneNumber = (EditText) findViewById(R.id.txtPhoneNumber);
+
+
+        btnStart = (Button) findViewById(R.id.btnStart);
+        btnStart.setOnClickListener(new View.OnClickListener(){
+
+            public void onClick(View v){
+                //perform the action on click
+                String phoneNumber = txtPhoneNumber.getText().toString();
+
+                String message = "this is a random message";
+
+                if (!phoneNumber.isEmpty()){
+
+                    Intent intentSMS = new Intent(Intent.ACTION_VIEW, Uri.parse("sms:"+ phoneNumber));
+                    intentSMS.putExtra("sms body", message);
+                    startActivity(intentSMS);
+
+                } else{
+
+                    new AlertDialog.Builder(MainActivity.this)
+                            .setTitle("Alert")
+                            .setMessage("Please insert a phone number before starting the protocol.")
+                            .setCancelable(false)
+                            .setPositiveButton("ok", new DialogInterface.OnClickListener() {
+                                @Override
+                                public void onClick(DialogInterface dialog, int which) {
+                                    //close the dialog
+                                    dialog.cancel();
+                                }
+                            }).show();
+
+                }
+
+            }
+
         });
     }
 
