54cfa06b27614048fc3a4aff213282fc38cc7804
Joana Chavez <chavezlucero88@gmail.com>
2017-04-16 22:05:29 +0200

    sending msg

--- app/src/main/java/com/example/smsencryption/smsencryption/MainActivity.java
+++ app/src/main/java/com/example/smsencryption/smsencryption/MainActivity.java
@@ -1,7 +1,12 @@
 package com.example.smsencryption.smsencryption;
 
+import android.app.Activity;
+import android.app.PendingIntent;
+import android.content.BroadcastReceiver;
+import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
+import android.content.IntentFilter;
 import android.net.Uri;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
@@ -9,16 +14,20 @@ import android.support.design.widget.Snackbar;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.telephony.SmsManager;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
     Button btnStart;
     EditText txtPhoneNumber;
+    EditText txtMessage;
+    EditText txtPin;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,6 +37,8 @@ public class MainActivity extends AppCompatActivity {
         setSupportActionBar(toolbar);
 
         txtPhoneNumber = (EditText) findViewById(R.id.txtPhoneNumber);
+        txtMessage = (EditText) findViewById(R.id.txtMessage);
+        txtPin = (EditText) findViewById(R.id.txtPin);
 
         btnStart = (Button) findViewById(R.id.btnStart);
         btnStart.setOnClickListener(new View.OnClickListener(){
@@ -38,11 +49,13 @@ public class MainActivity extends AppCompatActivity {
 
                 String message = "this is a random message";
 
-                if (!phoneNumber.isEmpty()){
+                if (phoneNumber.length()>0 && message.length()>0){
 
+                    /* This code is in case you want to send the message from the SMS app of the phone
                     Intent intentSMS = new Intent(Intent.ACTION_VIEW, Uri.parse("sms:"+ phoneNumber));
                     intentSMS.putExtra("sms body", message);
-                    startActivity(intentSMS);
+                    startActivity(intentSMS);*/
+                    sendSMS(phoneNumber, message);
 
                 } else{
 
@@ -57,12 +70,74 @@ public class MainActivity extends AppCompatActivity {
                                     dialog.cancel();
                                 }
                             }).show();
-
                 }
 
             }
 
         });
+    }
+
+    private void sendSMS(String phoneNumber, String message){
+
+        String SENT = "SMS_SENT";
+        String DELIVERED = "SMS_DELIVERED";
+
+        PendingIntent sentPI = PendingIntent.getBroadcast(this, 0,
+                new Intent(SENT), 0);
+
+        PendingIntent deliveredPI = PendingIntent.getBroadcast(this, 0,
+                new Intent(DELIVERED), 0);
+
+        //---when the SMS has been sent---
+        registerReceiver(new BroadcastReceiver(){
+            @Override
+            public void onReceive(Context arg0, Intent arg1) {
+                switch (getResultCode())
+                {
+                    case Activity.RESULT_OK:
+                        Toast.makeText(getBaseContext(), "SMS sent",
+                                Toast.LENGTH_SHORT).show();
+                        break;
+                    case SmsManager.RESULT_ERROR_GENERIC_FAILURE:
+                        Toast.makeText(getBaseContext(), "Generic failure",
+                                Toast.LENGTH_SHORT).show();
+                        break;
+                    case SmsManager.RESULT_ERROR_NO_SERVICE:
+                        Toast.makeText(getBaseContext(), "No service",
+                                Toast.LENGTH_SHORT).show();
+                        break;
+                    case SmsManager.RESULT_ERROR_NULL_PDU:
+                        Toast.makeText(getBaseContext(), "Null PDU",
+                                Toast.LENGTH_SHORT).show();
+                        break;
+                    case SmsManager.RESULT_ERROR_RADIO_OFF:
+                        Toast.makeText(getBaseContext(), "Radio off",
+                                Toast.LENGTH_SHORT).show();
+                        break;
+                }
+            }
+        }, new IntentFilter(SENT));
+
+        //---when the SMS has been delivered---
+        registerReceiver(new BroadcastReceiver(){
+            @Override
+            public void onReceive(Context arg0, Intent arg1) {
+                switch (getResultCode())
+                {
+                    case Activity.RESULT_OK:
+                        Toast.makeText(getBaseContext(), "SMS delivered",
+                                Toast.LENGTH_SHORT).show();
+                        break;
+                    case Activity.RESULT_CANCELED:
+                        Toast.makeText(getBaseContext(), "SMS not delivered",
+                                Toast.LENGTH_SHORT).show();
+                        break;
+                }
+            }
+        }, new IntentFilter(DELIVERED));
+
+        SmsManager sms = SmsManager.getDefault();
+        sms.sendTextMessage(phoneNumber, null, message, sentPI, deliveredPI);
     }
 
     @Override
@@ -86,4 +161,5 @@ public class MainActivity extends AppCompatActivity {
 
         return super.onOptionsItemSelected(item);
     }
+
 }
---
+++ app/src/main/java/com/example/smsencryption/smsencryption/SmsReceiver.java
@@ -0,0 +1,48 @@
+package com.example.smsencryption.smsencryption;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.os.Build;
+import android.os.Bundle;
+import android.provider.Telephony;
+import android.telephony.SmsMessage;
+import android.widget.Toast;
+
+/**
+ * Created by joana on 4/16/17.
+ */
+
+public class SmsReceiver extends BroadcastReceiver{
+
+    @Override
+    public void onReceive(Context context, Intent intent) {
+
+        //---get the SMS message passed in---
+        Bundle bundle = intent.getExtras();
+        SmsMessage[] msgs = null;
+        String str = "";
+        if (bundle != null) {
+            //---retrieve the SMS message received---
+            Object[] pdus = (Object[]) bundle.get("pdus");
+            msgs = new SmsMessage[pdus.length];
+            for (int i = 0; i < msgs.length; i++) {
+
+                //this has to be only for android versions < 19
+                if (Build.VERSION.SDK_INT < 19) {
+                    msgs[i] = SmsMessage.createFromPdu((byte[]) pdus[i]);
+                }else{
+                    //check if this works because this is only for the case sdk >=19
+                    msgs = Telephony.Sms.Intents.getMessagesFromIntent(intent);
+                }
+
+                str += "SMS from " + msgs[i].getOriginatingAddress();
+                str += " :";
+                str += msgs[i].getMessageBody().toString();
+                str += "n";
+            }
+            //---display the new SMS message---
+            Toast.makeText(context, str, Toast.LENGTH_SHORT).show();
+        }
+    }
+}
